658226db5641e905460707616a5623bdecddaed7
grevtsovweb <grevtsov.web@gmail.com>
2025-01-17 13:39:45 +0300

    Первый комит

---
+++ src/assets/js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+ 
+    const swiper = new Swiper('.calendar-section__block-slider--js', {
+        loop: false,
+        spaceBetween: 16,
+        slidesPerView: "1.1",
+      });
+
+})
